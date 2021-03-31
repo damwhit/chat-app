7bcaeb822d765cb456977fc21781b9a94a8801bc
David Whitaker <damwhit@gmail.com>
2021-03-31 17:42:28 -0400

    Implement client/server connection and configure cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// server-side
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
